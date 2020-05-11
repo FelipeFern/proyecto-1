3a113906751b7aa040cd8adbf02a0d444a0226dd
FelipeFern <felipefernandz@hotmail.com>
2020-05-11 18:57:03 -0300

    Analizador finalizado, junto con el almacenamiento de datos.

--- js/Analizador.js
+++ js/Analizador.js
@@ -6,53 +6,33 @@ var vocal = 0;
 var tilde = 0;
 var vowels = [97, 101, 105, 111, 117, 225, 233, 237, 243, 250];
 
-function contar(element) {
+function contar() {
 	var texto = $("textarea#TextoIngresado").val();
 	texto = texto.toLowerCase();
 
 	for (var i = 0; i < texto.length; i++) {
 		var c = texto.charCodeAt(i);
-		contarVocalTildeConsonante(c);
+		contarVocalTilde(c);
+		contarConsonanteCaracter(c);
 		contarPalabras(c, texto.charCodeAt(i - 1));
 	}
-
-	contarParrafos(texto);
-	contarPalabra(texto, "hol ");
-
 	if (c != "32" && c != "10") palabra++;
 
+	contarParrafos(texto);
+
 	mostrarResultados();
+	guardarDatos(texto, palabra, parrafo, caracter, vocal, consonante, tilde);
 	reiniciarVariables();
 }
 
-//Agregarla al boton cuando se quiera buscar algo.
-//Ya te devuelve el arreglo con todos los indices donde esta el elemento. Subrayarlos o marcarlos en el texto faltaria.
-function contarPalabra(texto, palabra) {
-	var reg = new RegExp(palabra, "g");
-	var n = 0;
-	if (texto.includes(palabra)) n = texto.match(reg).length;
-	console.log(n);
-	const length = (x) => x.length;
-	const sum = (a, b) => a + b;
-
-	const indexesOf = (substr) => ({
-		in: (str) =>
-			str
-				.split(substr)
-				.slice(0, -1)
-				.map(length)
-				.map((_, i, lengths) =>
-					lengths.slice(0, i + 1).reduce(sum, i * substr.length)
-				),
-	});
-	console.log(indexesOf(palabra).in(texto));
-}
-
-function contarVocalTildeConsonante(c) {
+function contarVocalTilde(c) {
 	if (vowels.includes(c)) {
 		vocal += 1;
 		if (c > 224) tilde += 1;
 	}
+}
+
+function contarConsonanteCaracter(c) {
 	if (c > 96 && c < 123 && !vowels.includes(c)) consonante += 1;
 	if (c != "32" && c != "10") caracter = caracter + 1;
 }
@@ -94,6 +74,50 @@ function reiniciarVariables() {
 	parrafo = 0;
 	palabra = 0;
 	consonante = 0;
-	vocales = 0;
+	vocal = 0;
 	tilde = 0;
+}
+
+function buscarPalabra() {
+	var texto = $("textarea#TextoIngresado").val().toLowerCase();
+	var palabra = $("textarea#aBuscar").val().toLowerCase();
+	var resultado = $("textarea#cantidad");
+
+	if (palabra == "") resultado.val("");
+	else {
+		var reg = new RegExp(palabra, "g");
+		var n = 0;
+
+		const length = (x) => x.length;
+		const sum = (a, b) => a + b;
+
+		const indexesOf = (substr) => ({
+			in: (str) =>
+				str
+					.split(substr)
+					.slice(0, -1)
+					.map(length)
+					.map((_, i, lengths) =>
+						lengths.slice(0, i + 1).reduce(sum, i * substr.length)
+					),
+		});
+
+		resultado.val(indexesOf(palabra).in(texto).length);
+		presentar(indexesOf(palabra).in(texto), texto, palabra);
+	}
+}
+
+function presentar(apariciones, texto) {
+	var modificado = "";
+	var i = 0;
+	var n = 25;
+	while (i <= apariciones.length - 1) {
+		if (apariciones[i] - n > 0) modificado += "...";
+		modificado += texto.substring(apariciones[i] - n, apariciones[i] + n);
+		if (apariciones[i] + n < texto.length) modificado += "..." + "\n";
+		else modificado += "\n";
+		i++;
+	}
+
+	$("#respuestas").val(modificado);
 }
--- js/temas.js
+++ js/temas.js
@@ -1,17 +1,8 @@
-// function to set a given theme/color-scheme
 function setTheme(themeName) {
 	localStorage.setItem("tema", themeName);
 	document.documentElement.className = themeName;
 }
-// function to toggle between light and dark theme
-function CambiarTema() {
-	if (localStorage.getItem("tema") === "tema-secundario") {
-		setTheme("tema-primario");
-	} else {
-		setTheme("tema-secundario");
-	}
-}
-// Immediately invoked function to set the theme on initial load
+
 (function () {
 	if (localStorage.getItem("tema") === "tema-secundario") {
 		setTheme("tema-secundario");
